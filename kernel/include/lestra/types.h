3cb8f27f0b70c2dcdd78c3fff98d1d583bc8f5a6
Lee Muriithi Kingori <leekingori54@gmail.com>
2026-07-01 08:07:07 +0300

    Update kernel/include/lestra/types.h

--- kernel/include/lestra/types.h
+++ kernel/include/lestra/types.h
@@ -6,21 +6,47 @@
 #ifndef LESTRA_TYPES_H
 #define LESTRA_TYPES_H
 
+#include <stdarg.h>
+
 /* Fixed-width integer types */
+#ifndef int8_t
 typedef signed char        int8_t;
+#endif
+#ifndef uint8_t
 typedef unsigned char      uint8_t;
+#endif
+#ifndef int16_t
 typedef signed short       int16_t;
+#endif
+#ifndef uint16_t
 typedef unsigned short     uint16_t;
+#endif
+#ifndef int32_t
 typedef signed int         int32_t;
+#endif
+#ifndef uint32_t
 typedef unsigned int       uint32_t;
+#endif
+#ifndef int64_t
 typedef signed long long   int64_t;
+#endif
+#ifndef uint64_t
 typedef unsigned long long uint64_t;
+#endif
 
 /* Size types */
+#ifndef size_t
 typedef unsigned long      size_t;
+#endif
+#ifndef ssize_t
 typedef signed long        ssize_t;
+#endif
+#ifndef intptr_t
 typedef long               intptr_t;
+#endif
+#ifndef uintptr_t
 typedef unsigned long      uintptr_t;
+#endif
 
 /* Boolean */
 typedef int                bool;
